165459dcaa67613dd8bd99433d64250859a8f643
JKTheRipperTH <69198341+JKTheRipperTH@users.noreply.github.com>
2022-09-05 02:31:55 +0700

    AUTOPROCESS NO LOCKPACK
    ไม่มีการเข้ารหัสไดๆ

---
+++ html/app.js
@@ -0,0 +1,23 @@
+
+window.onload = function () {
+	window.addEventListener('message', (event) => {
+		if (event.data.itemLabel) {
+			document.getElementById('img112').src = 'nui://nc_inventory/html/img/items/' + itemLabel + '.png';
+			document.getElementById('img2').src = 'nui://nc_inventory/html/img/items/' + itemgive + '.png';
+			document.getElementById('showProcess').style.display = 'block';
+		}
+	});
+	window.addEventListener('keyup', (event) => {
+		if (event.keyCode == 27) {
+			const el = document.getElementById('showProcess');
+			el.classList.remove('quality');
+			document.getElementById('showProcess').style.display = 'none';
+			fetch(`https://${GetParentResourceName()}/focusOff`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json; charset=UTF-8',
+				}
+			})
+		}
+	});
+}
